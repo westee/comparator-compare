7ab1697f22d297d0881b7a627ae79ce01546642c
dflk <904885508@qq.com>
2019-11-21 21:25:09 +0800

    使用Comparator完成比较

--- src/main/java/com/github/hcsp/polymorphism/Point.java
+++ src/main/java/com/github/hcsp/polymorphism/Point.java
@@ -1,7 +1,10 @@
 package com.github.hcsp.polymorphism;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Point {
@@ -53,7 +56,24 @@ public class Point {
 
     // 按照先x再y，从小到大的顺序排序
     // 例如排序后的结果应该是 (-1, 1) (1, -1) (2, -1) (2, 0) (2, 1)
-    public static List<Point> sort(List<Point> points) {}
+    public static List<Point> sort(List<Point> points) {
+//         Collections.sort(points, (o1, o2) -> {
+//            if (o1.x < o2.x){
+//                return 1;
+//            } else if(o1.x > o2.x){
+//                return -1;
+//            }
+//
+//            if(o1.y < o2.y){
+//                return  1;
+//            } else if(o1.y > o2.y){
+//                return -1;
+//            }
+//            return 0;
+//        });
+        Collections.sort(points, Comparator.comparing(Point::getX).thenComparing(Point::getY));
+         return points;
+    }
 
     public static void main(String[] args) throws IOException {
         List<Point> points =
@@ -63,6 +83,8 @@ public class Point {
                         new Point(1, -1),
                         new Point(2, 1),
                         new Point(2, -1));
+
+
         System.out.println(Point.sort(points));
     }
 }
